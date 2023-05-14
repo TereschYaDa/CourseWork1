3ad55005bf5a43992964a5152fac9ed7dc4ed4de
TereschYaDa <teresenko920@gmail.com>
2023-05-15 00:34:49 +0700

    final result (seems to be tested)

--- kursach.py
+++ kursach.py
@@ -15,14 +15,17 @@ class Product:
     def getGuarantee(self): return self.__guarantee
     def getPrice(self): return self.__price
     
-    def changePrice(self, share):     # rename to dicount
+    def discount(self, share):
         self.__setPrice(self.__price * (1 - share))
 
 class Store:
-    def __init__(self):
+    def __init__(self, size):
         self.__arr = []
         self._invalidateIterator()
+        self.__size = size
         self.__nextId = 0
+    
+    def getSize(self): return self.__size
 
     def _invalidateIterator(self):
         self.__i = -1
@@ -74,72 +77,79 @@ class Store:
             return False
 
     def filterByManufacturer(self, manufacturer):
-        filteredStore = Store()
-        for i in range(store.getQuantity): # ???
+        filteredStore = Store(12)
+        for i in range(self.getQuantity()):
             if manufacturer.lower() == self.__arr[i].getManufacturer().lower():
-                filteredStore.add(self.__arr[i])
+                filteredStore.add(self.__arr[i].getModel(), self.__arr[i].getManufacturer(), self.__arr[i].getGuarantee(), self.__arr[i].getPrice())
         return filteredStore
 
-    def changeAllPrices(self, share): # rename to discountAll
+    def discountAll(self, share):
         curIdx = self.__i
         elem = self.getFirstElem()
         while elem != None:
-            elem.changePrice(share)
+            elem.discount(share)
             elem = self.getNextElem()
         self.__i = curIdx
                     
 ###################################################################################
 '''Functions'''
 
-def printSelectionMenu():
+def processMainMenu():
     print('Выберите задачу:')
     print('0 - Выход из программы')
     print('1 - Вывод всех товаров               4 - Сортировка')
     print('2 - Добавление нового товара         5 - Фильтрация по производителю')
     print('3 - Удаление выбранного товара       6 - Преобразование цен')
+    responseForMenu = input()
+    return responseForMenu
 
-def instantiation(): # Using of global variables is making yourself cry sooner or later. Refactor to passing 'store' as a function parameter.
+def createStore():
+    store = Store(12)
     store.add('27EA63', 'LG', 12, 35000)
     store.add('BTM2360', 'Philips', 6, 9000)
     store.add('F1', 'Pocophone', 6, 23000)
     store.add('Fen', 'Dyson', 10, 10000)
     store.add('Ryzen 5 5600X', 'AMD', 18, 25000)
+    return store
 
 def AppendSpacesRight(string, targetLen): 
     return str(string) + ' ' * (abs(targetLen - len(str(string))) + 3) 
 
-def printTableRow(id, model, maxLenModel, manufacturer, maxLenManufacturer, guarantee, maxLenGuarantee, price, maxLenPrice):
-    print(' ' + id + '  ' + # use the same design pattern as for rest paramters: maxLen & AppendSpaces
+def printTableRow(id, maxLenId, model, maxLenModel, manufacturer, maxLenManufacturer, guarantee, maxLenGuarantee, price, maxLenPrice):
+    print(AppendSpacesRight(id, maxLenId) +
           AppendSpacesRight(model, maxLenModel) +
           AppendSpacesRight(manufacturer, maxLenManufacturer) +
           AppendSpacesRight(guarantee, maxLenGuarantee) +
           AppendSpacesRight(price, maxLenPrice))
 
 def printProductsTable(arr):
+    idHeader = "ID:"
     modelHeader = 'Модель:'
     manufacturerHeader = 'Производитель:'
     guaranteeHeader = 'Гарантия:'
     priceHeader = 'Цена:'
+    maxLenId = len(idHeader) + 1
     maxLenModel = len(modelHeader) + 1
     maxLenManufacturer = len(manufacturerHeader) + 1
     maxLenGuarantee = len(guaranteeHeader) + 1
     maxLenPrice = len(priceHeader) + 1
     elem = arr.getFirstElem()
     while elem != None:
+        if maxLenId < len(str(elem.getStoreId())): maxLenId = len(str(elem.getStoreId()))
         if maxLenModel < len(elem.getModel()): maxLenModel = len(elem.getModel())
         if maxLenManufacturer < len(elem.getManufacturer()): maxLenManufacturer = len(elem.getManufacturer())
         if maxLenGuarantee < len(str(elem.getGuarantee())): maxLenGuarantee = len(str(elem.getGuarantee()))
         if maxLenPrice < len(str(elem.getPrice())): maxLenPrice = len(str(elem.getPrice()))
         elem = arr.getNextElem()
 
-    printTableRow('ID:',
-                    modelHeader, maxLenModel,
-                    manufacturerHeader, maxLenManufacturer,
-                    guaranteeHeader, maxLenGuarantee,
-                    priceHeader, maxLenPrice)
+    printTableRow(idHeader, maxLenId,
+                  modelHeader, maxLenModel,
+                  manufacturerHeader, maxLenManufacturer,
+                  guaranteeHeader, maxLenGuarantee,
+                  priceHeader, maxLenPrice)
     elem = arr.getFirstElem()
     while elem != None:
-        printTableRow(elem.getStoreId(),
+        printTableRow(elem.getStoreId(), maxLenId,
                       elem.getModel(), maxLenModel,
                       elem.getManufacturer(), maxLenManufacturer,
                       elem.getGuarantee(), maxLenGuarantee,
@@ -147,82 +157,80 @@ def printProductsTable(arr):
         elem = arr.getNextElem()
     print()
 
-def processAddProduct():
-    if store.getQuantity() == 12: # Better move this constraint to parameter of Store class (passing to contsructor during creation). Explicit '12' should go to global constant to be used both here and in createStore().
-	# Don't use explicit '12' in text
-        print('В программе не может содержаться больше 12 товаров!\nЧтобы добавить новый товар, сначала удалите уже имеющийся. Ну или попросите программиста, ответсвенного за выполнение этого задания, чтобы не делал таких дурацких ограничеений :-D')
+def processAddProductMenu():
+    if store.getQuantity() == store.getSize():
+        print('В программе не может содержаться больше '+ store.getSize +' товаров!\nЧтобы добавить новый товар, сначала удалите уже имеющийся. Ну или попросите программиста, ответсвенного за выполнение этого задания, чтобы не делал таких дурацких ограничеений :-D')
     else:
         model = input('Введите название модели:\n')
         manufacturer = input('Введите название производителя:\n')
 	
-	correct = False
-	while not correct:
-		guarantee = input('Введите время гарантии(в месяцах):\n')
-		if guarantee.isdigit() == True: correct = True
-		else: print('Введены некорректные данные. Попробуйте еще раз.')
+    correct = False
+    while not correct:
+        guarantee = input('Введите время гарантии(в месяцах):\n')
+        if guarantee.isdigit() == True: correct = True
+        else: print('Введены некорректные данные. Попробуйте еще раз.')
 
-	correct = False
-	while not correct:
-	        price = input('Введите цену:\n')
-		if price.isdigit() == True: correct = True
-		else: print('Введены некорректные данные. Попробуйте еще раз.')
-        
-        store.add(model, manufacturer, guarantee, price)
+    correct = False
+    while not correct:
+        price = input('Введите цену:\n')
+        if price.isdigit() == True: correct = True
+        else: print('Введены некорректные данные. Попробуйте еще раз.')
+    store.add(model, manufacturer, guarantee, price)
     
-def processDeleteProduct():
-	# refactor same as in processAddProduct()
-    responseForDelete = input('Введите ID товара, который хотите удалить:\n')
-    while store.deleteElem(responseForDelete) != True:
-        print('Неправильный ввод.')
+def processDeleteProductMenu():
+    correct = False
+    while not correct:
         responseForDelete = input('Введите ID товара, который хотите удалить:\n')
+        if store.deleteElem(responseForDelete) != True: print('Неправильный ввод.')
+        else: correct = True
+    store.deleteElem(responseForDelete)
 
-def processSort():
-	# refactor same as in processAddProduct()
-    prop = input('Введите свойство товара, по которому хотите осуществить сортировку (модель, производитель, гарантия, цена):\n')
-    while store.sortBy(prop.lower()) != True:
-        print('Неправильный ввод.')
+def processSortMenu():
+    correct = False
+    while not correct:
         prop = input('Введите свойство товара, по которому хотите осуществить сортировку (модель, производитель, гарантия, цена):\n')
+        if store.sortBy(prop.lower()) != True: print('Неправильный ввод.')
+        else: correct = True
+    store.sortBy(prop)
 
-def processFilt():
+def processFilterProductsMenu():
     manufacturer = input('Введите производителя товара, по которому хотите осуществить фильрацию:\n')
-    if not store.filterByManufacturer(manufacturer): # double fitering, really?!
+    filteredStore = store.filterByManufacturer(manufacturer) 
+    if filteredStore.getQuantity() == 0:
         print('Товары производителя ' + manufacturer + ' отсутствуют\n')
     else:
-        printProductsTable(store.filterByManufacturer(manufacturer))
+        printProductsTable(filteredStore)
 
-def processChangePrice():
+def processDiscountProductsMenu():
     share = input('Введите процент, на который хотите снизить цену всех товаров:\n')
     if share.isdigit():
         share = int(share)
         share /= 100
-        store.changeAllPrices(share)
+        store.discountAll(share)
     else:
         print('Неправильный ввод.')
-        processChangePrice()
+        processDiscountProductsMenu()
 
 ###################################################################################
 '''Executing code'''
 
-store = Store() # move creation to init, making smth like store = createStore()
-instantiation() # rename to createStore()
+store = createStore()
 responseForMenu = ''
 print('Вас приветствует приложение "Магазин бытовой техники"\n')
 while responseForMenu != '0':
-    printSelectionMenu() # better include asking for input into printSelectionMenu() (and thus rename function to smth like processMainMenu)
-    responseForMenu = input() # see above, goes to responseForMenu = processMainMenu()
+    responseForMenu = processMainMenu()
     if responseForMenu == '1':
         if store.getQuantity() > 0:
             printProductsTable(store)
         else:
             print('Нет товаров')
-    elif responseForMenu == '2': processAddProduct() # rename to processAddProductMenu
+    elif responseForMenu == '2': processAddProductMenu()
     elif responseForMenu == '3':
         printProductsTable(store)
-        processDeleteProduct() # same, add ...Menu
-    elif responseForMenu == '4': processSort() # same, add ...Menu
-    elif responseForMenu == '5':
-        processFilt()  # rename to processFilterProductsMenu
-    elif responseForMenu == '6': processChangePrice()  # rename to processDiscountProductsMenu
+        processDeleteProductMenu()
+    elif responseForMenu == '4': processSortMenu()
+    elif responseForMenu == '5': processFilterProductsMenu()
+    elif responseForMenu == '6': processDiscountProductsMenu()
     elif responseForMenu == '0': print('До свидания!')
     else: print('Вероятно, ввод был выполнен неправильно. Попробуйте еще раз:')
     
